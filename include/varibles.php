862eaabb99cbed9336369ca5c1c874bf61cc955e
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 01:39:13 +0300

    Update varibles.php

--- include/varibles.php
+++ include/varibles.php
@@ -1,17 +1,5 @@
 <?php
 
-include 'Telegram.class.php';
-
-$input = file_get_contents('php://input');
-$update = json_decode($input);
-$admin = 000000000;// your id
-$token = ''; //bot token
-$telegram = new Telegram($token);
-$message = $update->message;
-$chat_id = $message->chat->id;
-$message_id = $message->message_id;
-$text = $message->text;
-$data = $update->callback_query->data;
 
 
 ?>
