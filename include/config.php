1cd196aa0d9a238b7428958e9e6a8394b1ee9e0d
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 01:39:31 +0300

    Update config.php

--- include/config.php
+++ include/config.php
@@ -45,4 +45,17 @@ $time = date('H:i');
 $date = date('d.m.Y');
 
 
+$input = file_get_contents('php://input');
+$update = json_decode($input);
+$admin = 000000000;// your id
+$token = ''; //bot token
+$telegram = new Telegram($token);
+$message = $update->message;
+$chat_id = $message->chat->id;
+$message_id = $message->message_id;
+$text = $message->text;
+$data = $update->callback_query->data;
+
+
+
 ?>
