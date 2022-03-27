04b29626c98fc7cabb643c27af38c6236c3ccf19
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 00:13:20 +0300

    Update varibles.php

--- include/varibles.php
+++ include/varibles.php
@@ -1,38 +1,17 @@
 <?php
 
-// PHP Input
-$update = json_decode(file_get_contents('php://input'));
+include 'class/Telegram.class.php';
 
-// For Text
+$input = file_get_contents('php://input');
+$update = json_decode($input);
+$admin = 000000000;// your id
+$token = ''; //bot token
+$telegram = new Telegram($token);
 $message = $update->message;
-$cid = $message->chat->id;
-$mid = $message->message_id;
-$uid = $message ->from->id;
+$chat_id = $message->chat->id;
+$message_id = $message->message_id;
 $text = $message->text;
-$contact = $message->contact;
-$phone_number = $contact->phone_number;
+$data = $update->callback_query->data;
 
-// User Property
-$name = $message->from->first_name;
-
-// For CallBack Query
-$query = $update->callback_query;
-$data = $query->data;
-$uid2 = $query->query->id;
-$cid2 = $query->message->chat->id;
-$mid2 = $query->message->message_id;
-$ccid = $query->message->chat->id;
-$cuid = $query->message->from->id;
-$cqid = $query->id;
-$cqidd = $query->from->id;
-$qid = $query->id;
-$name2 = $query->from->first_name;
-$photo = $message->photo;
-$file_id = $message->file_id;
-$location = $message->location;
-$pre_check = $update->pre_checkout_query;
-$pre_id = $pre_check->id;
-$ship_chek = $update->shipping_query;
-$ship_id = $ship_chek->id;
 
 ?>
