db1f4ccac5e71c46864e938da68760afa7c30b62
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 01:28:13 +0300

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,9 +1,29 @@
 <?php
 
 $token = ""; // TOKEN of BOT
-
 define('API_KEY',"$token"); // Username of BOT
-
 $admin = "518962398"; // Admin Telegram ID
+
+
+class Telegram {
+    private $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    private function bot($method , $data){
+        $url = 'https://api.telegram.org/bot'.$this->token.'/'.$method;
+        $ch = curl_init($url);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER , TRUE);
+        curl_setopt($ch , CURLOPT_POSTFIELDS , $data);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($res);
+    }
+
 
 ?>
