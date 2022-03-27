39f35a3cb3b256d6007a09600ac342f2da4abc02
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 01:27:23 +0300

    Update functions.php

--- include/functions.php
+++ include/functions.php
@@ -1,25 +1,4 @@
 <?php
-
-class Telegram {
-    private $token;
-
-    public function __construct($token)
-    {
-        $this->token = $token;
-    }
-
-    private function bot($method , $data){
-        $url = 'https://api.telegram.org/bot'.$this->token.'/'.$method;
-        $ch = curl_init($url);
-
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER , TRUE);
-        curl_setopt($ch , CURLOPT_POSTFIELDS , $data);
-
-        $res = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($res);
-    }
-
     public function sendMessage($chat_id , $text , $reply = null , $parse_mode = 'MarkDown'){
         return $this->bot('sendMessage' , [
             'chat_id' => $chat_id,
