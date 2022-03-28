2a89fa08134ef6fb573bdb56f17c8ce00a04fe24
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 06:02:00 +0300

    Update functions.php

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,47 @@
 <?php
     
+public function sendMessage($chat_id , $text , $reply = null , $parse_mode = 'MarkDown'){
+        return $this->bot('sendMessage' , [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'reply_markup' => $reply,
+            'parse_mode' => $parse_mode
+        ]);
+    }
 
+    public function forwardMessage($from , $to , $message_id){
+        return $this->bot('forwardMessage' , [
+            'chat_id'  => $to,
+            'from_chat_id' => $from,
+            'message_id' => $message_id,
+        ]);
+    }
+    public function copyMessage($from , $to , $message_id , $reply = null,$reply_to_message_id = null){
+        return $this->bot('copyMessage' , [
+            'chat_id'  => $to,
+            'from_chat_id' => $from,
+            'message_id' => $message_id,
+            'reply_markup' => $reply,
+            'reply_to_message_id' => $reply_to_message_id
+        ]);
+    }
+
+    public function answerCallbackQuery($cb_id , $text , $alert){
+        return $this->bot('answerCallbackQuery',[
+            'callback_query_id' => $cb_id,
+            'text' => $text,
+            'show_alert' => $alert
+        ]);
+    }
+    function edit_replay($chatid , $msgid ,$reply){
+		return $this->bot('editMessageReplyMarkup',[
+            'chat_id'=>$chatid,
+            'message_id'=>$msgid,
+            'reply_markup'=>$reply
+		]);
+	}
+
+    
+}
 
 ?>
