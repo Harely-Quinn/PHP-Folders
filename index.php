fe1bea4a4d3592de342a748cd394539f853a1e82
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 10:47:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,20 +11,15 @@ $chat_id = $message->chat->id;
 $message_id = $message->message_id;
 $text = $message->text;
 $data = $update->callback_query->data;
-$lang_path = 'data/'. $chat_id . '.txt';
-$lang = 'en';
-if(file_exists($lang_path)){
-    $lang = file_get_contents($lang_path);
-}
 
-include 'lang/'.$lang.'.php';
+
 
 if($text == '/start' and $chat_id == $admin){
         $lang_btn = json_encode(['inline_keyboard' => [
             [['text' => 'English🇬🇧' , 'callback_data' => 'lang-en']],
             [['text' => 'Persian🇮🇷' , 'callback_data' => 'lang-fa']]
         ]]);
-        $telegram->sendMessage($chat_id ,$txt['s_lang'], $lang_btn );
+        $telegram->sendMessage($chat_id ,"hello", $lang_btn );
     }
 
 if($data != null){
@@ -32,7 +27,7 @@ if($data != null){
     $mid = $update->callback_query->message->message_id;
        
         $cb_id = $update->callback_query->id;
-        $telegram->answerCallbackQuery($cb_id , $txt['lang_changed'],true);
+        $telegram->answerCallbackQuery($cb_id , "Ok",true);
         $telegram->sendMessage($userid , $txt['restart']);
     }
 
