7ea881ed8712016720f092c3bd0c6a296d6ff642
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-28 10:00:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,6 @@ $chat_id = $message->chat->id;
 $message_id = $message->message_id;
 $text = $message->text;
 $data = $update->callback_query->data;
-$lang_path = 'data/'. $chat_id . '.txt';
-$lang = 'en';
-if(file_exists($lang_path)){
-    $lang = file_get_contents($lang_path);
-}
 
 if($text == '/start'){
     if(!file_exists($lang_path)){
